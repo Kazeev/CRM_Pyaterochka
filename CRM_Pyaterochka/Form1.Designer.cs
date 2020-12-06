287a70c865d0f3d0d9004a9db84b8f41d6705e10
Kazeev <misha_kazeev@mail.ru>
2020-12-07 00:06:39 +0300

    Initial commit

---
+++ CRM_Pyaterochka/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace CRM_Pyaterochka
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.настройкToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьСотрудникаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.убратьСотрудникаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сменаПароляToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.расчётToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.посчитатьВырочкуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сотрудникаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сменыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.продуктыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьПродуктToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьПрордуктToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настроитьПродуктToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NameUsers = new System.Windows.Forms.Label();
+            this.PunchTheGoods = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.настройкToolStripMenuItem, this.расчётToolStripMenuItem, this.продуктыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // настройкToolStripMenuItem
+            // 
+            this.настройкToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.добавитьСотрудникаToolStripMenuItem, this.убратьСотрудникаToolStripMenuItem, this.сменаПароляToolStripMenuItem, this.выходToolStripMenuItem});
+            this.настройкToolStripMenuItem.Name = "настройкToolStripMenuItem";
+            this.настройкToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.настройкToolStripMenuItem.Text = "Настройк";
+            // 
+            // добавитьСотрудникаToolStripMenuItem
+            // 
+            this.добавитьСотрудникаToolStripMenuItem.Name = "добавитьСотрудникаToolStripMenuItem";
+            this.добавитьСотрудникаToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.добавитьСотрудникаToolStripMenuItem.Text = "Добавить сотрудника";
+            // 
+            // убратьСотрудникаToolStripMenuItem
+            // 
+            this.убратьСотрудникаToolStripMenuItem.Name = "убратьСотрудникаToolStripMenuItem";
+            this.убратьСотрудникаToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.убратьСотрудникаToolStripMenuItem.Text = "Убрать сотрудника";
+            // 
+            // сменаПароляToolStripMenuItem
+            // 
+            this.сменаПароляToolStripMenuItem.Name = "сменаПароляToolStripMenuItem";
+            this.сменаПароляToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.сменаПароляToolStripMenuItem.Text = "Смена пароля ";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // расчётToolStripMenuItem
+            // 
+            this.расчётToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.посчитатьВырочкуToolStripMenuItem});
+            this.расчётToolStripMenuItem.Name = "расчётToolStripMenuItem";
+            this.расчётToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.расчётToolStripMenuItem.Text = "Расчёт";
+            // 
+            // посчитатьВырочкуToolStripMenuItem
+            // 
+            this.посчитатьВырочкуToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.сотрудникаToolStripMenuItem, this.сменыToolStripMenuItem});
+            this.посчитатьВырочкуToolStripMenuItem.Name = "посчитатьВырочкуToolStripMenuItem";
+            this.посчитатьВырочкуToolStripMenuItem.Size = new System.Drawing.Size(183, 22);
+            this.посчитатьВырочкуToolStripMenuItem.Text = "Посчитать вырочку";
+            // 
+            // сотрудникаToolStripMenuItem
+            // 
+            this.сотрудникаToolStripMenuItem.Name = "сотрудникаToolStripMenuItem";
+            this.сотрудникаToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.сотрудникаToolStripMenuItem.Text = "Сотрудника";
+            // 
+            // сменыToolStripMenuItem
+            // 
+            this.сменыToolStripMenuItem.Name = "сменыToolStripMenuItem";
+            this.сменыToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.сменыToolStripMenuItem.Text = "Смены";
+            // 
+            // продуктыToolStripMenuItem
+            // 
+            this.продуктыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.добавитьПродуктToolStripMenuItem, this.удалитьПрордуктToolStripMenuItem, this.настроитьПродуктToolStripMenuItem});
+            this.продуктыToolStripMenuItem.Name = "продуктыToolStripMenuItem";
+            this.продуктыToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.продуктыToolStripMenuItem.Text = "Продукты";
+            // 
+            // добавитьПродуктToolStripMenuItem
+            // 
+            this.добавитьПродуктToolStripMenuItem.Name = "добавитьПродуктToolStripMenuItem";
+            this.добавитьПродуктToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.добавитьПродуктToolStripMenuItem.Text = "Добавить продукт";
+            // 
+            // удалитьПрордуктToolStripMenuItem
+            // 
+            this.удалитьПрордуктToolStripMenuItem.Name = "удалитьПрордуктToolStripMenuItem";
+            this.удалитьПрордуктToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.удалитьПрордуктToolStripMenuItem.Text = "Удалить прордукт";
+            // 
+            // настроитьПродуктToolStripMenuItem
+            // 
+            this.настроитьПродуктToolStripMenuItem.Name = "настроитьПродуктToolStripMenuItem";
+            this.настроитьПродуктToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.настроитьПродуктToolStripMenuItem.Text = "Настроить продукт";
+            // 
+            // NameUsers
+            // 
+            this.NameUsers.Location = new System.Drawing.Point(657, 24);
+            this.NameUsers.Name = "NameUsers";
+            this.NameUsers.Size = new System.Drawing.Size(131, 187);
+            this.NameUsers.TabIndex = 2;
+            this.NameUsers.Text = "Имя";
+            this.NameUsers.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // PunchTheGoods
+            // 
+            this.PunchTheGoods.Location = new System.Drawing.Point(12, 415);
+            this.PunchTheGoods.Name = "PunchTheGoods";
+            this.PunchTheGoods.Size = new System.Drawing.Size(149, 23);
+            this.PunchTheGoods.TabIndex = 3;
+            this.PunchTheGoods.Text = "Пробить товар ";
+            this.PunchTheGoods.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(615, 370);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.PunchTheGoods);
+            this.Controls.Add(this.NameUsers);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        private System.Windows.Forms.Button PunchTheGoods;
+
+        private System.Windows.Forms.Label NameUsers;
+
+        private System.Windows.Forms.ToolStripMenuItem настроитьПродуктToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьПродуктToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьСотрудникаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem посчитатьВырочкуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem продуктыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem расчётToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сменаПароляToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сменыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сотрудникаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem убратьСотрудникаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьПрордуктToolStripMenuItem;
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+
+        #endregion
+    }
+}
---
+++ CRM_Pyaterochka/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using Npgsql;
+
+namespace CRM_Pyaterochka
+{
+    public partial class Form1 : Form
+    {
+        NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;" + "Password=admin;Database=CRM_Pyaterochka;");
+        public Form1()
+        {
+            InitializeComponent();
+            
+            conn.Open(); 
+            NpgsqlDataReader dr = new NpgsqlCommand("select * from \"Pyaterochka\".goods", conn).ExecuteReader();
+            while (dr.Read())
+            {
+                
+            }
+            conn.Close();
+        }
+
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
